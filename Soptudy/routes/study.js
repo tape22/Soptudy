243f05b4762b7c6684990bf0ed376e352fcb3e0f
chaeppy <chaeppy108@gmail.com>
2020-08-15 03:47:23 +0900

    uri에 study/ 추가

--- Soptudy/routes/study.js
+++ Soptudy/routes/study.js
@@ -3,23 +3,24 @@ const studyContoller = require('../controllers/studyController')
 var router = express.Router();
 
 
-//전체, 태그별 스터디 조회 페이지
-router.get('/home/:tagCode', studyContoller.getHome)
 
-//스터디 등록 페이지
-router.post('/register', studyContoller.register)
+//전체, 카테고리별 스터디 조회 페이지
+router.get('/:category', studyContoller.getStudies)
 
-//스터디 신청 페이지
-router.get('/apply/:studyIdx', studyContoller.getApply)
-router.post('/apply', studyContoller.postApply)
+//스터디 등록
+// router.post('/register', studyContoller.registerStudy)
 
-//스터디 수정 페이지
-router.get('/edit/:studyIdx', studyContoller.getEdit)
-router.put('/edit/:studyIdx', studyContoller.putEdit)
-router.post('/passwd/:studyIdx', studyContoller.passwd)
+//스터디 상세 정보 조회
+router.get('/detail/:studyId', studyContoller.getStudyDetail)
 
-//임시 테스트
-router.post('/user', studyContoller.user)
+//특정 스터디 신청
+router.post('/apply', studyContoller.applyStudy)
+
+//스터디 정보 수정
+// router.put('/edit/:studyId', studyContoller.editStudyDetail)
+
+//스터디 비밀번호 확인
+router.post('/password', studyContoller.checkPassword)
 
 
 module.exports = router;
