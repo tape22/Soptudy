9f2fbbb2d377666fe6ff834329855a66a65e68b7
chaeppy <chaeppy108@gmail.com>
2020-08-15 18:57:26 +0900

    editStudy, registerStudy 추가

--- Soptudy/controllers/studyController.js
+++ Soptudy/controllers/studyController.js
@@ -1,5 +1,8 @@
 const userModel = require('../models/user');
-const studyModel = require('../models/study')
+const studyModel = require('../models/study');
+const { isfullHeadCount } = require('../models/study');
+const OPEN_STUDY = true;
+const CLOSE_STUDY = false;
 
 const study = {
     // home : 전체-0, 분야별-1,2,3,4,5 스터디 조회
@@ -13,17 +16,13 @@ const study = {
                 studyInfo = await studyModel.searchAllStudy();
             }
 
-            console.log(studyInfo);
-
-
-
             return res.status(200).json({
                 message: "Success",
                 data: studyInfo
             });
 
         } catch (err) {
-            console.log('home Err: ', err);
+            console.log('getStudies Err: ', err);
             return res.status(500).json({
                 message: "Server Error",
                 err: err
@@ -32,15 +31,65 @@ const study = {
     },
 
     //registerStudy: 스터디 등록
-    //registerStudy: async (req, res) => {
-    //     const {}= req.body;
-    //     try {
-    //         const result = await studyModel.addStudy(req.body)
-    //         res.status(200).send(result);
-    //     } catch (err) {
-    //         res.status(500).send(err)
-    //     }
-    // },
+    registerStudy: async (req, res) => {
+        const {
+            icon,
+            category,
+            leaderName,
+            leaderPhoneNumber,
+            leaderPart,
+            title,
+            intro,
+            content,
+            schedule,
+            location,
+            headCount,
+            password
+        } = req.body;
+        try {
+            if (!icon || !category || !leaderName || !leaderPhoneNumber || !leaderPart || !title || !intro || !content || !schedule || !location || !headCount || !password) {
+                console.log('registerStudy: missing required value');
+                return res.status(400).json({
+                    message: "Missing required value"
+                });
+            }
+
+            let user = await userModel.findUser(leaderPhoneNumber);
+            if (!user) {
+                user = await userModel.addUser({
+                    name : leaderName,
+                    phoneNumber: leaderPhoneNumber,
+                    part: leaderPart
+                });
+            }
+
+            const result = await studyModel.addStudy({
+                icon,
+                category,
+                leader: user._id,
+                title,
+                intro,
+                content,
+                schedule,
+                location,
+                headCount,
+                password,
+                status: true
+            })
+
+            return res.status(200).json({
+                message: "Success",
+                data: result
+            });
+
+        } catch (err) {
+            console.log('registerStudy Err: ', err);
+            return res.status(500).json({
+                message: "Server Error",
+                err: err
+            });
+        }
+    },
 
     // getStudyDetail: 특정 스터디 정보 조회  (파람필요)
     getStudyDetail: async (req, res) => {
@@ -73,32 +122,41 @@ const study = {
 
         try {
             if (!studyId || !name || !phoneNumber) {
-                console.log('postApply null value');
+                console.log('applyStudy: Missing required value');
                 return res.status(400).json({
-                    message: "Required: studyId, name, phoneNumber"
+                    message: "Missing required value"
                 });
             }
 
             let user = await userModel.findUser(phoneNumber);
             if (!user) {
-                user = await userModel.addUser({studyId,name,phoneNumber,part,bound});
+                user = await userModel.addUser({
+                    name,
+                    phoneNumber,
+                    part,
+                    bound
+                });
             }
 
-            const isExistMember =  await studyModel.findMember(studyId,user._id);
-            if(isExistMember !== null){
+            const isExistMember = await studyModel.findMember(studyId, user._id);
+            if (isExistMember !== null) {
                 return res.status(400).json({
-                    message: '이미 등록된 멤버입니다.'
+                    message: 'Already applied member'
                 });
             }
 
             const result = await studyModel.addMember(studyId, user._id)
+
+            if( await studyModel.isfullHeadCount(studyId) === true ){
+                await studyModel.modifyStudyStatus(studyId,CLOSE_STUDY);
+            }//result data에는 update된 status 반영안됨. 
+
             return res.status(200).json({
                 message: "Success",
                 data: result
             });
-
         } catch (err) {
-            console.log('postApply Err : ', err)
+            console.log('applyStudy Err : ', err)
             return res.status(500).json({
                 message: "Server Error",
                 err: err
@@ -107,24 +165,91 @@ const study = {
     },
 
     //editStudyDetail: 특정 스터디 정보 수정 (파람 필요)
+    editStudyDetail: async (req, res) => {
+        const studyId = req.params.studyId;
+        const {
+            icon,
+            category,
+            leaderName,
+            leaderPhoneNumber,
+            leaderPart,
+            title,
+            intro,
+            content,
+            schedule,
+            location,
+            headCount
+        } = req.body;
+
+        try {
+            if (!icon || !category || !leaderName || !leaderPhoneNumber || !leaderPart || !title || !intro || !content || !schedule || !location || !headCount) {
+                console.log('editStudyDetail: missing required value');
+                return res.status(400).json({
+                    message: "Missing required value"
+                });
+            }
+
+            let user = await userModel.findUser(leaderPhoneNumber);
+            if (!user) {
+                user = await userModel.addUser({
+                    name : leaderName,
+                    phoneNumber: leaderPhoneNumber,
+                    part: leaderPart
+                });
+            }
+
+            const result = await studyModel.modifyStudy(studyId,{
+                icon,
+                category,
+                leader: user._id,
+                title,
+                intro,
+                content,
+                schedule,
+                location,
+                headCount
+            })
+
+            if( await studyModel.isfullHeadCount(studyId) === true ){
+                await studyModel.modifyStudyStatus(studyId,CLOSE_STUDY);
+            }else{
+                await studyModel.modifyStudyStatus(studyId,OPEN_STUDY);
+            } //result data에는 update된 status 반영안됨.
+
+            return res.status(200).json({
+                message: "Success",
+                data: result
+            });
+
+        } catch (err) {
+            console.log('editStudyDetail Err: ', err);
+            return res.status(500).json({
+                message: "Server Error",
+                err: err
+            });
+        }
+    },
 
     //checkPassword: 특정스터디 비밀번호 확인
-    checkPassword: async (req,res) => {
-        const { studyId, password }= req.body;
+    checkPassword: async (req, res) => {
+        const {
+            studyId,
+            password
+        } = req.body;
         try {
-            const result = await studyModel.checkPassword(studyId,password);
+            const result = await studyModel.checkPassword(studyId, password);
 
-            if( result === false ){
+            if (result === false) {
                 return res.status(400).json({
                     message: "fail"
                 });
-            }else{
+            } else {
                 return res.status(200).json({
                     message: "Success"
                 });
             }
         } catch (err) {
-            console.log('password Err: ', err);
+            console.log('checkPassword Err: ', err);
             return res.status(500).json({
                 message: "Server Error",
                 err: err
