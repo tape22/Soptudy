04b339815224789c3c75a3915f614c58f3a705fd
chaeppy <chaeppy108@gmail.com>
2020-08-17 03:16:34 +0900

    응답 메세지 추가

--- Soptudy/modules/responseMessage.js
+++ Soptudy/modules/responseMessage.js
@@ -1,4 +1,17 @@
 module.exports = {
-    NULL_VALUE: '필요한 값이 없습니다',
-    OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
+    SERVER_ERROR: '서버 오류',
+    NULL_VALUE: '필요한 값이 없습니다.',
+    OUT_OF_VALUE: '파라미터 값이 잘못되었습니다.',
+
+    STUDY_CATEGORY_SUCCESS: '분야별 스터디 조회 성공',
+    STUDY_DETAIL_SUCCESS: '스터디 상세정보 조회 성공',
+
+
+    STUDY_REGISTER_SUCCESS: '스터디 등록 완료',
+    STUDY_APPLY_SUCCESS: '스터디 신청 완료',
+    STUDY_APPLY_FAIL: '이미 신청한 스터디 입니다.',
+    STUDY_EDIT_SUCCESS: '스터디 수정 완료',
+
+    MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+    PASSWORD_CHECK_SUCCESS: '비밀번호 확인 성공'
 };
