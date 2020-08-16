86f1ba4e271960938fc9a2f4e03e7b9d4fbbb12a
chaeppy <chaeppy108@gmail.com>
2020-08-17 03:15:56 +0900

    response코드에 util,statusCode,resMessage 적용

--- Soptudy/controllers/studyController.js
+++ Soptudy/controllers/studyController.js
@@ -1,57 +1,38 @@
 const userModel = require('../models/user');
 const studyModel = require('../models/study');
-const { isfullHeadCount } = require('../models/study');
+const encrypt = require('../modules/crypto');
+const util = require('../modules/util');
+const statusCode = require('../modules/statusCode');
+const resMessage = require('../modules/responseMessage');
 const OPEN_STUDY = true;
 const CLOSE_STUDY = false;
 
 const study = {
-    // home : 전체-0, 분야별-1,2,3,4,5 스터디 조회
-    getStudies: async (req, res) => {
+    //분야별 스터디 조회 
+    //0-전체, 1-기획, 2-디자인, 3-서버, 4-안드로이드, 5-ios, 6-웹
+    getStudyByCategory: async (req, res) => {
         const category = req.params.category;
-        let studyInfo;
+        let studiesInfo;
         try {
             if (category != 0) {
-                studyInfo = await studyModel.searchStudy(category);
+                studiesInfo = await studyModel.searchStudyByCategory(category);
             } else {
-                studyInfo = await studyModel.searchAllStudy();
+                studiesInfo = await studyModel.searchStudyAll();
             }
-
-            return res.status(200).json({
-                message: "Success",
-                data: studyInfo
-            });
-
+            return res.status(statusCode.OK).json(util.success(statusCode.OK,resMessage.STUDY_CATEGORY_SUCCESS,studiesInfo));
         } catch (err) {
-            console.log('getStudies Err: ', err);
-            return res.status(500).json({
-                message: "Server Error",
-                err: err
-            });
+            console.log('getStudyByCategory Err: ', err);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR,resMessage.SERVER_ERROR));
         }
     },
 
-    //registerStudy: 스터디 등록
+    //스터디 등록
     registerStudy: async (req, res) => {
-        const {
-            icon,
-            category,
-            leaderName,
-            leaderPhoneNumber,
-            leaderPart,
-            title,
-            intro,
-            content,
-            schedule,
-            location,
-            headCount,
-            password
-        } = req.body;
+        const { icon, category, leaderName, leaderPhoneNumber, leaderPart, title, intro, content, schedule, location, headCount, password } = req.body;
         try {
-            if (!icon || !category || !leaderName || !leaderPhoneNumber || !leaderPart || !title || !intro || !content || !schedule || !location || !headCount || !password) {
-                console.log('registerStudy: missing required value');
-                return res.status(400).json({
-                    message: "Missing required value"
-                });
+            if ( !icon || !category || !leaderName || !leaderPhoneNumber || !leaderPart || !title || !intro || !content || !schedule || !location || !headCount || !password ) {
+                console.log('registerStudy: null value');
+                return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST,resMessage.NULL_VALUE));
             }
 
             let user = await userModel.findUser(leaderPhoneNumber);
@@ -63,69 +44,37 @@ const study = {
                 });
             }
 
-            const result = await studyModel.addStudy({
-                icon,
-                category,
-                leader: user._id,
-                title,
-                intro,
-                content,
-                schedule,
-                location,
-                headCount,
-                password,
-                status: true
-            })
-
-            return res.status(200).json({
-                message: "Success",
-                data: result
-            });
-
+            const { salt, hashed } = await encrypt.encrypt(password);
+            await studyModel.addStudy({ icon, category, leader: user._id, title, intro, content, schedule, location, headCount, password: hashed, salt: salt, status: OPEN_STUDY });
+            
+            return res.status(statusCode.OK).json(util.success(statusCode.OK,resMessage.STUDY_REGISTER_SUCCESS));
         } catch (err) {
             console.log('registerStudy Err: ', err);
-            return res.status(500).json({
-                message: "Server Error",
-                err: err
-            });
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR,resMessage.SERVER_ERROR));
         }
     },
 
-    // getStudyDetail: 특정 스터디 정보 조회  (파람필요)
+    //특정 스터디 정보 조회
     getStudyDetail: async (req, res) => {
         const studyId = req.params.studyId;
         try {
-            const result = await studyModel.getOneStudy(studyId);
-            return res.status(200).json({
-                message: "Success",
-                data: result
-            });
+            const studyInfo = await studyModel.searchStudyDetail(studyId);
+            return res.status(statusCode.OK).json(util.success(statusCode.OK,resMessage.STUDY_DETAIL_SUCCESS,studyInfo));
         } catch (err) {
             console.log('getApply Err: ', err);
-            return res.status(500).json({
-                message: "Server Error",
-                err: err
-            });
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR,resMessage.SERVER_ERROR));
         }
     },
 
 
-    // applyStudy: 특정 스터디 신청
+    //특정 스터디 신청
     applyStudy: async (req, res) => {
-        const {
-            studyId,
-            name,
-            phoneNumber,
-            part,
-            bound,
-        } = req.body;
+        const { studyId, name, phoneNumber, part } = req.body;
 
         try {
-            if (!studyId || !name || !phoneNumber) {
-                console.log('applyStudy: Missing required value');
-                return res.status(400).json({
-                    message: "Missing required value"
-                });
+            if ( !studyId || !name || !phoneNumber ) {
+                console.log('applyStudy: null value');
+                return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST,resMessage.NULL_VALUE));
             }
 
             let user = await userModel.findUser(phoneNumber);
@@ -140,53 +89,31 @@ const study = {
 
             const isExistMember = await studyModel.findMember(studyId, user._id);
             if (isExistMember !== null) {
-                return res.status(400).json({
-                    message: 'Already applied member'
-                });
+                return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST,resMessage.STUDY_APPLY_FAIL));
             }
 
-            const result = await studyModel.addMember(studyId, user._id)
+            await studyModel.addMember(studyId, user._id)
 
             if( await studyModel.isfullHeadCount(studyId) === true ){
                 await studyModel.modifyStudyStatus(studyId,CLOSE_STUDY);
-            }//result data에는 update된 status 반영안됨. 
+            }
 
-            return res.status(200).json({
-                message: "Success",
-                data: result
-            });
+            return res.status(statusCode.OK).json(util.success(statusCode.OK,resMessage.STUDY_APPLY_SUCCESS));
+
         } catch (err) {
             console.log('applyStudy Err : ', err)
-            return res.status(500).json({
-                message: "Server Error",
-                err: err
-            })
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR,resMessage.SERVER_ERROR));
         }
     },
 
-    //editStudyDetail: 특정 스터디 정보 수정 (파람 필요)
+    //스터디 정보 수정
     editStudy: async (req, res) => {
         const studyId = req.params.studyId;
-        const {
-            icon,
-            category,
-            leaderName,
-            leaderPhoneNumber,
-            leaderPart,
-            title,
-            intro,
-            content,
-            schedule,
-            location,
-            headCount
-        } = req.body;
-
+        const { icon, category, leaderName, leaderPhoneNumber, leaderPart, title, intro, content, schedule, location, headCount } = req.body;
         try {
-            if (!icon || !category || !leaderName || !leaderPhoneNumber || !leaderPart || !title || !intro || !content || !schedule || !location || !headCount) {
-                console.log('editStudyDetail: missing required value');
-                return res.status(400).json({
-                    message: "Missing required value"
-                });
+            if ( !icon || !category || !leaderName || !leaderPhoneNumber || !leaderPart || !title || !intro || !content || !schedule || !location || !headCount ) {
+                console.log('editStudy: null value');
+                return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST,resMessage.NULL_VALUE));
             }
 
             let user = await userModel.findUser(leaderPhoneNumber);
@@ -198,17 +125,7 @@ const study = {
                 });
             }
 
-            const result = await studyModel.modifyStudy(studyId,{
-                icon,
-                category,
-                leader: user._id,
-                title,
-                intro,
-                content,
-                schedule,
-                location,
-                headCount
-            })
+            await studyModel.modifyStudy(studyId, {icon, category, leader: user._id, title, intro, content, schedule, location, headCount})
 
             if( await studyModel.isfullHeadCount(studyId) === true ){
                 await studyModel.modifyStudyStatus(studyId,CLOSE_STUDY);
@@ -216,44 +133,28 @@ const study = {
                 await studyModel.modifyStudyStatus(studyId,OPEN_STUDY);
             } //result data에는 update된 status 반영안됨.
 
-            return res.status(200).json({
-                message: "Success",
-                data: result
-            });
+            return res.status(statusCode.OK).json(util.success(statusCode.OK,resMessage.STUDY_EDIT_SUCCESS));
 
         } catch (err) {
-            console.log('editStudyDetail Err: ', err);
-            return res.status(500).json({
-                message: "Server Error",
-                err: err
-            });
+            console.log('editStudy Err: ', err);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR,resMessage.SERVER_ERROR));
         }
     },
 
-    //checkPassword: 특정스터디 비밀번호 확인
+    //스터디 비밀번호 확인
     checkPassword: async (req, res) => {
-        const {
-            studyId,
-            password
-        } = req.body;
+        const {studyId, password} = req.body;
         try {
             const result = await studyModel.checkPassword(studyId, password);
 
             if (result === false) {
-                return res.status(400).json({
-                    message: "fail"
-                });
+                return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST,resMessage.MISS_MATCH_PW));
             } else {
-                return res.status(200).json({
-                    message: "Success"
-                });
+                return res.status(statusCode.OK).json(util.success(statusCode.OK,resMessage.PASSWORD_CHECK_SUCCESS));
             }
         } catch (err) {
             console.log('checkPassword Err: ', err);
-            return res.status(500).json({
-                message: "Server Error",
-                err: err
-            });
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR,resMessage.SERVER_ERROR));
         }
     }
 }
