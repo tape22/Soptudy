799559b9f6b9b216fb8d0a0eaa1f80912d60bdd2
tape22 <gjwjdlas@gmail.com>
2020-08-19 17:13:01 +0900

    메인화면 최종디자인 변경(1)

--- Soptudy/routes/index.js
+++ Soptudy/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: '솝터디' });
 });
 
+router.get('/opened.ejs', function (req, res, next) {
+  res.render('opened', { title: '스터디 개설하기' });
+});
+
 module.exports = router;
