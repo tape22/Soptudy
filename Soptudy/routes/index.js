42dcb10e5be73e4a104f043ebab7471c2dddf59d
tape22 <gjwjdlas@gmail.com>
2020-08-31 23:31:34 +0900

    카드 목업

--- Soptudy/routes/index.js
+++ Soptudy/routes/index.js
@@ -3,8 +3,14 @@ const studyContoller = require('../controllers/studyController');
 const { routes } = require('../app');
 var router = express.Router();
 
+// router.use('./study', require('./study'));
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: '솝터디' });
+});
 
-router.use('./study', require('./study'));
-
+router.get('/opened.ejs', function (req, res, next) {
+  res.render('opened', { title: '스터디 개설하기' });
+});
 
 module.exports = router;
